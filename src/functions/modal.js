681c242a4b373a8cd94ce0367127eb32e6897668
dongkp96 <dongkp96@gmail.com>
2025-01-25 17:21:47 -0500

    added functionality for item creation modal buttons and added item create modal

--- src/functions/modal.js
+++ src/functions/modal.js
@@ -10,3 +10,16 @@ export function projectModal(){
         createProjectDialog.close();
     })
 }
+
+export function itemModal(){
+    const createItemDialog = document.querySelector("#item-creator");
+    const showItemFormBtn = document.querySelector("#add-item");
+    showItemFormBtn.addEventListener("click", ()=>{
+        createItemDialog.showModal();
+    }) 
+
+    const closeItemFormBtn = document.querySelector("#close2");
+    closeItemFormBtn.addEventListener("click", ()=>{
+        createItemDialog.close();
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import "./styles.css";
-import {projectModal} from "./functions/modal.js";
+import {projectModal, itemModal} from "./functions/modal.js";
 
 projectModal();
+itemModal();
 const submitProjectBtn = document.querySelector("#submit");
 submitProjectBtn.addEventListener("click", ()=>{
     const element = document.querySelector("#project-name");
     alert(element.value);
+})
+
+const submitItemBtn = document.querySelector("#submit-item");
+submitItemBtn.addEventListener("click", ()=>{
+    const element = document.querySelector("#task-name");
+    alert(element.value)
 })
